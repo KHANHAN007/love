1c457ced37ac8fc8fb9dcf3965b02195176ba05d
KHANHAN007 <khanhanke@gmail.com>
2024-11-24 01:03:19 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,19 @@
+
+const CONFIG = {
+    introTitle: 'Vợ Linh ơi!',
+    introDesc: `Anh yêu em
+    Thương em
+    Nhớ em`,
+    btnIntro: 'Em yêu anh',
+    title: 'Có yêu anh không 😙',
+    desc: 'Có nhớ anh không ',
+    btnYes: 'Cóoooooooo, em yêu anh, em nhớ anh nhiều!!! <33',
+    btnNo: 'Không yêu anh đâu :3',
+    question: 'Nói anh nghe, em yêu, em nhớ như nào<3',
+    btnReply: 'Gửi cho anh <3',
+    reply: 'Nói lời trực tiếp với anh vợ nhé <33333333',
+    mess: 'Anh nhớ em nhiều lắm 🥰. Thương em nhiều nhiều 😘😘 ',
+    messDesc: 'Cho anh ôm thật chặt, hôn anh thật lâu nhé',
+    btnAccept: 'Vâng ạ <3',
+    messLink: 'https://www.messenger.com/e2ee/t/8570911686281717?__pwa=1'
+}
